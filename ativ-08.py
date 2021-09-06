c4636b7c761071c56d60b3879a1e575f4dc194d2
Arthur da Silva Rodrigues <silva.arthur2506@gmail.com>
2021-09-06 16:39:31 -0300

    Create ativ-08.py

---
+++ ativ-08.py
@@ -0,0 +1,5 @@
+PC = ['gabinete cooler master','GeForce RTX 2080Ti','Intel Core i5 10400','HD de 1 TB e 8 GB','Asus EX-B460M-V5','Monitor','Mouse','Teclado']
+print(PC)
+PC.remove('gabinete cooler master')
+PC.remove('GeForce RTX 2080Ti')
+print(PC)
