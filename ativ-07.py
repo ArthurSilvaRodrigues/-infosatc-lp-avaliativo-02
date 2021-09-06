060e59c5032ec3fdc4f7cd09e2c7f12b5b84d15c
Arthur da Silva Rodrigues <silva.arthur2506@gmail.com>
2021-09-06 16:39:27 -0300

    Create ativ-07.py

---
+++ ativ-07.py
@@ -0,0 +1,14 @@
+filmes =   ['Homem-aranha','Titanic','Se beber não case','Até que a morte nos separe']
+jogos  =   ['GTA','FIFA','Fortnite','Minicrft']
+livros =   ['50 tons de cinza','O diario de Anne Frank','A cabana']
+esportes = ['Basquete','Salto em distancia','Revesamento 100x100','Fotebol']
+#--------------------------------------------------------------------------------------------------
+tListas = [filmes,jogos,livros,esportes]
+#--------------------------------------------------------------------------------------------------
+print(livros[2])
+#--------------------------------------------------------------------------------------------------
+esportes.remove('Basquete')
+#--------------------------------------------------------------------------------------------------
+diciplinas = ['Matematica','Portugues','Ingles','Historia']
+tListas.append(diciplinas)
+print(tListas)
